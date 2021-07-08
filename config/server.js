de2c10e2569d23204b9629bed397102d53dae0b7
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-07-08 13:49:35 -0700

    init example

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0fd66bd873f33bb5211d522b90016fa4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a9eb027c-0ade-4bb0-834a-146e2d4507af'
+};
